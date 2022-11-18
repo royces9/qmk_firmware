bbacf40736d4559327b301cc66993564c86c6c5c
Royce <rrysato@gmail.com>
2022-11-18 23:58:40 +0900

    edit readme a bit add some comments

--- keyboards/converter/pc9800_kb/setup_pc9800_kb.h
+++ keyboards/converter/pc9800_kb/setup_pc9800_kb.h
@@ -4,14 +4,18 @@
 
 #define BAUD_RATE 19200
 
+//RTY on D0
+//RDY on D6
+//RST on D7
+
+#define RTY_HIGH() (PORTD |= (1U << PORTD0))
+#define RTY_LOW() (PORTD &= ~(1U << PORTD0))
+
 #define RDY_HIGH() (PORTD |= (1U << PORTD6))
 #define RDY_LOW() (PORTD &= ~(1U << PORTD6))
 
 #define RST_HIGH() (PORTD |= (1U << PORTD7))
 #define RST_LOW() (PORTD &= ~(1U << PORTD7))
-
-#define RTY_HIGH() (PORTD |= (1U << PORTD0))
-#define RTY_LOW() (PORTD &= ~(1U << PORTD0))
 
 #define IS_MAKE(byte) (~(byte) & 0x80)
 
